1775d2c96769ce91f90a4149b7c2aecc602547d5
Hamza <hamzabman@gmail.com>
2020-02-19 16:56:01 -0500

    Modify navbar add some text

---
+++ src/components/about.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+    state = {  }
+    render() { 
+        return (  
+        <div class="row align-items-start" className = "aboutme" id="1">
+            <h1>Hamza Saleem</h1>
+            <h2>Software Developer</h2>
+        </div>
+        );
+    }
+}
+ 
+export default About;
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
-import { Navbar, NavItem, NavDropdown, MenuItem, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import { Navbar, NavItem, NavDropdown, MenuItem, Nav, Form, FormControl, Button, Container} from 'react-bootstrap';
+import { Link, animateScroll as scroll } from "react-scroll";
 class Header extends Component {
     state = {  }
     render() { 
-        return (
-        <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home"></Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                <Nav.Link href="#home">About</Nav.Link>
-                <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-                <Nav.Link href="#resume">Resume</Nav.Link>
-                <Nav.Link href="#contact">Contact</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar> 
+        return (     
+        <Container>
+            <Navbar bg="light" expand="lg">
+            <Nav className="ml-auto">
+                    <Nav.Link href="#home">About</Nav.Link>
+                    <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+                    <Nav.Link href="#resume">Resume</Nav.Link>
+                    <Nav.Link href="#contact">Contact</Nav.Link>
+                    </Nav>   
+            </Navbar> 
+        </Container>
         );
     }
 }
