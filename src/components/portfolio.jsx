8d4ec3ae3be869d0d720b66af15bf4924fa1ede7
hamzabman@gmail.com <hamzabman@gmail.com>
2020-04-16 23:06:11 -0400

    Finalize portfolio cards

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -6,28 +6,54 @@ class Portfolio extends Component {
     return (
       <div className="row align-items-center" id="thirdRow">
         <div className="col" id="project">
-          <h2>portfolio</h2>
+          <h2>
+            portfolio{"\t\t\t"}
+            <a href="https://github.com/Hamza-S/portfolio">
+              <i class="fa fa-github" id="icon2"></i>
+            </a>{" "}
+          </h2>
           <p>
-            An nba score app that tells you live stats and scores from ongoing
-            games in the NBA, as well as the leaders for today
+            A website for my personal portfolio that showcases the projects I
+            have worked on.
           </p>
+          <div className="techs">
+            <h9 className="tag">React</h9>
+            <h9 className="tag">JavaScript</h9>
+          </div>
         </div>
         <div className="col" id="project">
-          <a href="https://github.com/Hamza-S">
-            <i class="fa fa-github" id="icons"></i>
-          </a>
-          <h2>nba-hub</h2>
+          <h2>
+            nba-hub{" "}
+            <a href="https://github.com/Hamza-S">
+              <i class="fa fa-github" id="icon2"></i>
+            </a>{" "}
+          </h2>
           <p>
-            An nba score app that tells you live stats and scores from ongoing
-            games in the NBA, as well as the leaders for today
+            Access statistics and information on NBA teams and players, as well
+            as view live score from games.
           </p>
+          <div className="techs">
+            <h9 className="tag">Python</h9>
+            <h9 className="tag">React</h9>
+            <h9 className="tag">NodeJS</h9>
+            <h9 className="tag">Express</h9>
+            <h9 className="tag">SQL</h9>
+          </div>
         </div>
         <div className="col" id="project">
-          <h2>blog</h2>
-          <p>
-            An nba score app that tells you live stats and scores from ongoing
-            games in the NBA, as well as the leaders for today
-          </p>
+          <h2>
+            blog{" "}
+            <a href="https://github.com/Hamza-S">
+              <i class="fa fa-github" id="icon2"></i>
+            </a>{" "}
+          </h2>
+          <p>Personal blog site used to share my ideas</p>
+          <div className="techs">
+            <h9 className="tag">React</h9>
+            <h9 className="tag">NodeJS</h9>
+            <h9 className="tag">Express</h9>
+            <h9 className="tag">SQL</h9>
+          </div>
         </div>
       </div>
     );
