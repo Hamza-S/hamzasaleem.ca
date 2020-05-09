a91d30c8d0b6f538bae6afac6752fe7beae3cabc
hamzabman@gmail.com <hamzabman@gmail.com>
2020-05-08 20:04:26 -0400

    Display fetched posts from db in React

--- api/routes/get_posts.js
+++ api/routes/get_posts.js
@@ -4,17 +4,11 @@ var router = express.Router();
 var pgp = require("pg-promise")(/* options */);
 var db = pgp("postgres://postgres:postgres@localhost:5432/blog");
 /* GET users listing. */
+
 router.get("/", function (req, res, next) {
   db.any("SELECT id,title,date,body FROM posts")
     .then(function (data) {
-      const str = JSON.stringify(data[0]);
-      const obj = JSON.parse(str);
-      const id = obj.id;
-      const title = obj.title;
-      const date = obj.date;
-      const body = obj.body;
-      res.send(body);
-      console.log(data[0]);
+      res.json(data);
     })
     .catch(function (error) {
       console.log("ERROR:", error);
--- frontend/src/blog/PostContainer.jsx
+++ frontend/src/blog/PostContainer.jsx
@@ -1,24 +1,66 @@
 import React, { Component } from "react";
-import Post from "./Post.jsx";
+import ReactHtmlParser, {
+  processNodes,
+  convertNodeToElement,
+  htmlparser2,
+} from "react-html-parser";
+
 class PostContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { posts: [] };
+  }
+
+  fetchPosts() {
+    fetch("http://localhost:9000/get_posts")
+      .then((res) => res.json())
+      .then((res) => this.setState({ posts: res }));
+  }
+
+  componentDidMount() {
+    this.fetchPosts();
+  }
+
+  processPosts() {
+    let titles = this.state.posts
+      .slice(0)
+      .reverse()
+      .map(function (posts) {
+        return [
+          <div className="row align-items-center" id="blogfirstRow">
+            <h1 id="postTitle"> {posts.title}</h1>
+            <h2 id="postDate">{posts.date}</h2>
+            {ReactHtmlParser(posts.body)}
+          </div>,
+        ];
+      });
+    return titles;
+  }
+
   render() {
-    return (
-      <div className="container" id="postContainer">
-        <div className="row align-items-center" id="blogfirstRow">
-          <h1 id="title">
-            <b>My Blog</b>
-          </h1>
-          <p id="intro">
-            Welcome to my blog! My name is Hamza I am an undergraduate student
-            at York University studying Computer Science. This blog consists of
-            posts ranging from world issues, television shows, movies, software
-            development, or really anything on my mind that I want to talk
-            about.
-          </p>
+    if (!this.state.posts) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div className="container" id="postContainer">
+          <div className="row align-items-center" id="blogfirstRow">
+            <h1 id="title">
+              <b>My Blog</b>
+            </h1>
+            <p id="intro">
+              Welcome to my blog! My name is Hamza I am an undergraduate student
+              at York University studying Computer Science. This blog consists
+              of posts ranging from world issues, television shows, movies,
+              software development, or really anything on my mind that I want to
+              talk about.
+            </p>
+          </div>
+
+          {this.processPosts()}
         </div>
-        <Post />
-      </div>
-    );
+      );
+    }
   }
 }
 
