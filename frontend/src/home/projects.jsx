0693153bed4f68cd2dd6afcf8158107b62cbf4ed
Hamza <hamzabman@gmail.com>
2020-09-13 01:40:45 -0400

    Add tech tags to project cards

--- frontend/src/home/projects.jsx
+++ frontend/src/home/projects.jsx
@@ -5,15 +5,21 @@ export const Projects = () => {
     <div id="myProjects">
       <h1 id="projectSection">Projects</h1>
       <div id="myProject">
+        <h3>hamzasaleem.ca & personal blog</h3>
+        <p>This website which is used to portray my skills and projects!</p>
+        <ul id="techList">
+          <li id="techTag">Node.js</li>
+          <li id="techTag">React</li>
+          <li id="techTag">JavaScript</li>
+          <li id="techTag">PostgreSQL</li>
+        </ul>
+      </div>
+      <div id="myProject">
         <h3>hamzasaleem.ca</h3>
         <p>This website which is used to portray my skills and projects!</p>
       </div>
       <div id="myProject">
-        <h4>hamzasaleem.ca</h4>
-        <p>This website which is used to portray my skills and projects!</p>
-      </div>
-      <div id="myProject">
-        <h4>hamzasaleem.ca</h4>
+        <h3>hamzasaleem.ca</h3>
         <p>This website which is used to portray my skills and projects!</p>
       </div>
     </div>
