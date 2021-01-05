13c08407097c34b805ba965012849222dca403cf
Hamza <hamzabman@gmail.com>
2021-01-05 14:19:43 -0500

    Update footer

--- frontend/src/home/footer.jsx
+++ frontend/src/home/footer.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export const Footer = () => {
   return (
     <div className="col" id="myFooter">
-      <h3>Updated as of Sep 2020</h3>
+      <h3>Updated as of Jan 2021</h3>
       <h7 class="iconCredits"></h7>Website designed and created by Hamza Saleem{" "}
       <p>
         Icon Credits:{" "}
